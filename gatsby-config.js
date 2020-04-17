7104acf4ee00cac9ae0ca7e64f70e66c2e01b48b
Robert Veloso <robert@velosodigital.com>
2020-04-17 14:13:35 -0300

    Update sharp dependencie

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,20 +16,20 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `psus.com.br`,
+        short_name: `psus`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.svg`,
       },
     },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: 'gatsby-plugin-root-import',
       options: {
